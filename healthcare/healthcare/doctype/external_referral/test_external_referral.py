5f09b9165faa14f0fffacc3b5f7046aa35133580
kinyanjui <steve@upande.com>
2026-07-21 12:12:29 +0300

    feat: add External Referral doctype with client script

---
+++ healthcare/healthcare/doctype/external_referral/test_external_referral.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, earthians Health Informatics Pvt. Ltd. and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestExternalReferral(FrappeTestCase):
+	pass
